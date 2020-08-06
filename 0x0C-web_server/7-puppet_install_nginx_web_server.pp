146f558af16d83b804cbb1a49dca4af3efac79cb
mfcrespo <mafe_crespo@hotmail.com>
2020-08-06 05:51:55 +0000

    remove comments

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,23 @@
-# Configure Nginx whit puppet
+# Install and configure an Nginx server
 
-# sudo apt-get update y sudo apt-get -y install nginx
-package { 'nginx':
+package { 'nginx web server':
   ensure => installed,
   name   => 'nginx'
 }
 
-# echo "Holberton School" > /var/www/html/index.html
 file { '/var/www/html/index.html':
   ensure  => present,
   path    => '/var/www/html/index.html',
   content => 'Holberton School'
 }
 
-# REDIRECT="/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;"
-# sudo sed -i "$REDIRECT" /etc/nginx/sites-available/default
-file_line { 'redirect':
+file_line { 'redirect to 301':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
 }
 
-# sudo service nginx restart
 service { 'nginx':
   ensure     => running,
   require    => Package['nginx'],
